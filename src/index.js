d79dc18641df42ef62d6bc283b07f1ddcfa07001
cjp0421 <pedersencarol1@gmail.com>
2021-06-16 12:07:17 -0500

    updated geolocation weather button
    Weather description now updates when current location button is pressed

--- src/index.js
+++ src/index.js
@@ -123,6 +123,7 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
+      axios.get(apiUrl).then(displayWeatherDescriptionDefault);
 }
 
 function getCurrentLocation(event) {
